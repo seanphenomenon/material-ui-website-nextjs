2018426f599aaa8b8025884b979e642da4f379cf
seanphenomenon <seanvharrell@gmail.com>
2020-11-15 16:29:04 -0800

    testing deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 //this file includes  packages that gives us the ability to extend the functionality/middleware/customization of next.js setup (github page)
-const withPlugins = require('next-compose-plugins'); //  this plug-in allows us to take optimized images and packages them all together
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins"); //  this plug-in allows us to take optimized images and packages them all together
+const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([
-    [optimizedImages, {
+  [
+    optimizedImages,
+    {
       /* config for next-optimized-images */
-    }],
+    },
+  ],
+
+  // your other plugins here
+  {
+    target: "serverless",
+  },
   
-    // your other plugins here
-  
-  ]);
+]);
